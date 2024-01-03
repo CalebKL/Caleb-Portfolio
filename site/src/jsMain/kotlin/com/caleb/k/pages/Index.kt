9abc08e4e9d4ba91f768bea6ce9f5e99c647a6ed
CalebK <langat.caleb25@gmail.com>
2024-01-03 15:25:29 +0300

    page updates

--- site/src/jsMain/kotlin/com/caleb/k/pages/Index.kt
+++ site/src/jsMain/kotlin/com/caleb/k/pages/Index.kt
@@ -1,18 +1,38 @@
 package com.caleb.k.pages
 
 import androidx.compose.runtime.*
+import com.caleb.k.components.BackToTopButton
+import com.caleb.k.components.OverflowMenu
+import com.caleb.k.sections.*
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.core.Page
-import org.jetbrains.compose.web.dom.Text
 
 @Page
 @Composable
 fun HomePage() {
-    // TODO: Replace the following with your own content
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("THIS PAGE INTENTIONALLY LEFT BLANK")
+    var menuOpened by remember { mutableStateOf(false) }
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            MainSection(onMenuClicked = { menuOpened = true })
+            AboutSection()
+            ServiceSection()
+            ExperienceSection()
+            PortfolioSection()
+            ContactSection()
+            FooterSection()
+        }
+        BackToTopButton()
+        if (menuOpened) {
+            OverflowMenu(onMenuClosed = { menuOpened = false })
+        }
     }
 }
---
+++ site/src/jsMain/kotlin/com/caleb/k/utils/Constants.kt
@@ -0,0 +1,140 @@
+package com.caleb.k.utils
+
+
+object Constants {
+    const val WEBSITE = ""
+    const val SECTION_WIDTH = 1920
+    const val FONT_FAMILY = "Roboto"
+
+    const val UPWORK_DESC =
+        "- As a self-employed Android Developer operating on platforms such as Upwork,\n" +
+                " I brought a wealth of expertise in Kotlin, Jetpack Compose,\n" +
+                " Data binding, XML, Unit testing, UI Testing, Clean Architecture,\n" +
+                " and collaboration to various projects. \n" +
+                "Key highlights of my freelance role include working on user interfaces from \n " +
+                "Figma and Adobe using both Jetpack Compose and XML, maintaining legacy code \n" +
+                "by fixing bugs and migrating to latest app architecture patterns such as MVP to \n" +
+                "MVVM (Model-View-ViewModel), app modularization, and migrating legacy code to modern\n" +
+                " practices like Kotlin coroutines (for concurrency), Dagger Hilt for \n" +
+                "dependency injection (DI) and Retrofit. I also worked with outside data sources and\n" +
+                " API Third Parties, maintained standards in android development using clean\n" +
+                " architectural design patterns, and wrote unit tests for the code including edge cases,\n" +
+                " usability, and general reliability, deploying on the Playstore periodically."
+    const val GREENSTAND_OVERVIEW =
+        "- In my role at Greenstand, " +
+                "I've been an integral part of a dynamic engineering team, contributing to the " +
+                "innovative reforestation initiatives by leveraging cutting-edge technology." +
+                " Collaborating with a talented group of engineers, our focus has been on empowering " +
+                "thousands of farmers through a digital tree tracking system, ensuring they receive " +
+                "compensation for their vital contributions to reforestation. Responsible for " +
+                "maintaining legacy applications, I address bugs and implement improvements to guarantee" +
+                " stability, maintaining it at a level greater than 80%. A significant aspect of my" +
+                " responsibilities involves enhancing the unit test coverage of the code base from 15%" +
+                " to approximately 65%, while also implementing CI pipelines to ensure the overall " +
+                "stability of the app. Working closely with the design team, I actively participate in " +
+                "breaking down user requirements, creating user personas, developing requirement " +
+                "canvases, and defining technical requirements and roadmaps. My collaboration extends " +
+                "to product managers, QA, UI/UX, and backend teams to deliver products with clean user " +
+                "interfaces, and I play a pivotal role in developing stable, data-driven, and" +
+                " product-driven Android features for the application."
+
+    const val STAX_OVERVIEW =
+        "- At Stax Payments, I worked as an OPEN SOURCE Developper playing a pivotal role in" +
+                " maintaining and advancing our cutting-edge" +
+                " payment processing solution, which excels in automating USSD transactions for businesses." +
+                " My responsibilities span various aspects, encompassing networking, data persistence," +
+                " and user interfaces utilizing both XML and Jetpack Compose. A key focus has been the " +
+                "modularization of the Stax App into a multi-module application, aligning with " +
+                "SOLID principles to enhance scalability and maintainability. Collaborating closely " +
+                "with team members, I actively contribute to improving unit test code coverage, " +
+                "elevating it from 20% to 35%. As part of the evolution of our app architecture, I " +
+                "helped lead the migration from a singular module to a multi-modular structure, " +
+                "incorporating " +
+                "best practices. Additionally, I champion the introduction of superior practices such as" +
+                " dependency injection using Dagger 2, facilitating a more robust and efficient codebase." +
+                " The implementation of automated testing, employing techniques like " +
+                "test-driven development, has been a key initiative, ensuring the reliability and " +
+                "quality of our codebase."
+    const val POCKET_CAST_OVERVIEW =
+        "- At Pocket Casts, I work as an Open Source Developer being responsible for assisting the mobile team in developing the native mobile app entirely in Kotlin, adhering to Modern Android Best Practices. This involved migrating from XML to Jetpack Compose, ensuring we followed best practices for a modern declarative approach to UI .\n" +
+                "\n" +
+                "I helped maintained the stability of the legacy applications by fixing bugs and releasing improvements, aiming for a stability rate of over 85%. Additionally, I worked towards increasing the test coverage from approximately 50% to 65%, which helped ensure the reliability of our codebase.\n" +
+                "\n" +
+                "One of my key responsibilities was migrating legacy code to modern practices. This included adopting Kotlin coroutines for handling concurrency, Dagger Hilt for dependency injection, and migrating to Jetpack Compose. These modern practices improved the efficiency and readability of our codebase.\n" +
+                "\n" +
+                "Overall, my role at Pocket Casts was crucial in maintaining the stability and functionality of our applications while continuously improving them with modern technologies and practices."
+    const val COMMUNICATION_SKILLS =
+        "- Proven ability to convey complex ideas clearly and concisely, fostering effective collaboration within teams.\n" +
+                "Strong interpersonal skills, facilitating open communication and understanding within teams. " +
+                "Proficient in both written and oral English communication."
+    const val SOFT_SKILLS =
+        "- Possess a range of interpersonal skills such as teamwork, adaptability, and time management, contributing to a positive and productive work atmosphere.\n" +
+                "Adept at problem-solving and critical thinking, demonstrating resilience and flexibility in dynamic work scenarios."
+    const val KOTLIN_OVERVIEW =
+        "- Expertise in Kotlin programming language, leveraging its concise syntax and modern features to " +
+                "enhance the development of Android applications.\n" +
+                "Capable of seamlessly transitioning between Java and Kotlin, ensuring code readability, " +
+                "leveraging Kotlin Coroutines for asynchronous and concurrent programming, " +
+                "and optimizing performance in diverse coding environments."
+    const val ANDROID_OVERVIEW =
+        "- Extensive experience in Android app development, including proficiency in Jetpack Compose " +
+                "for crafting modern, declarative user interfaces and a solid understanding of the" +
+                " Android View system. My expertise extends to encompass a comprehensive knowledge of most" +
+                " Android platform APIs, allowing for seamless integration of diverse functionalities and " +
+                "features into applications."
+    const val GENERAL_SOFTWARE_SKILLS =
+        "- As a general rule of thumb, while developing Android Applications, I adhere to, " +
+                "SOLID principles, emphasizing a modular and scalable code structure. " +
+                "I also prioritize code reuse, to enhance efficiency and reduce redundancy. " +
+                "Additionally, I place a strong emphasis on code maintainability, employing thorough documentation, " +
+                "testing, and code reviews to guarantee a resilient and easily updatable codebase. "
+    const val EXPERIMENTING_OVERVIEW =
+        "- Currently experimenting with Compose HTML, and Compose for Desktop. " +
+                "Also, learning a lot about Kotlin Multiplatform."
+
+    const val SHORT_INTRO =
+        "I am an Experienced and resourceful Android Engineer primarily working with Kotlin. My strongest skills include: Debugging, Refactoring, Researching and Learning New and Upcoming Technologies."
+    const val ABOUT_ME_1 =
+        "- A Software Engineer with a strong background in developing Native Mobile Applications. \n" +
+                "My expertise encompasses developing robust Android applications while applying\n " +
+                "SOLID principles, having a good Architecture and applying security best practice\n" +
+                "to produce impeccable code."
+    const val ABOUT_ME_2 =
+        "- My passion for technology is matched by a commitment to social impact, demonstrated through\n" +
+                "intensive participation of Open Source Technologies and community based initiatives.\n" +
+                "I am also a strong advocate for scalable and modular system architecture with intensive\n" +
+                "testing. I have co-facilitated transitions to more efficient tech stacks,\n" +
+                " significantly improving system performance and adaptability. "
+    const val ABOUT_ME_3 =
+        "- A career marked by continuous learning and innovation, I am dedicated to leveraging technology\n" +
+                "to create meaningful solutions that resonate with users and contribute to societal progress.\n" +
+                "In addition to my technical skills, I value effective communication and collaboration within teams\n" +
+                "and thrive in an agile environment where adaptability is key."
+    const val LOREM_IPSUM_SHORT =
+        "- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+}
+
+object Res {
+    object Icon {
+        const val web = "web_icon.svg"
+        const val seo = "seo_icon.svg"
+        const val android = "android_icon.svg"
+        const val link = "link_icon.svg"
+        const val shield = "shield_icon.svg"
+        const val checkmark = "checkmark_icon.svg"
+        const val star = "star_icon.svg"
+    }
+
+    object Image {
+        const val background = "background.svg"
+        const val logo = "logo.svg"
+        const val main = "main_image.png"
+        const val about = "Android.jpeg"
+        const val map = "mapImage.jpeg"
+        const val chillmax = "chillmax.jpeg"
+        const val diary = "diary.jpeg"
+        const val harryporter = "harryporter.jpeg"
+        const val ricky = "ricky.jpeg"
+        const val landing = "landing.jpeg"
+    }
+}
