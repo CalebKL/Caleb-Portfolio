42d9c9dcd6d34e051d6b7337cc8614dd23c7db5c
CalebK <langat.caleb25@gmail.com>
2024-01-03 15:24:45 +0300

    adds MainSection

---
+++ site/src/jsMain/kotlin/com/caleb/k/sections/MainSection.kt
@@ -0,0 +1,179 @@
+package com.caleb.k.sections
+
+import androidx.compose.runtime.Composable
+import com.caleb.k.components.Header
+import com.caleb.k.components.SocialBar
+import com.caleb.k.models.Section
+import com.caleb.k.models.Theme
+import com.caleb.k.styles.MainButtonStyle
+import com.caleb.k.styles.MainImageStyle
+import com.caleb.k.utils.Constants.FONT_FAMILY
+import com.caleb.k.utils.Constants.SECTION_WIDTH
+import com.caleb.k.utils.Constants.SHORT_INTRO
+import com.caleb.k.utils.Res
+import com.varabyte.kobweb.compose.css.*
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
+import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.layout.SimpleGrid
+import com.varabyte.kobweb.silk.components.layout.numColumns
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.components.style.toModifier
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.Button
+import org.jetbrains.compose.web.dom.P
+import org.jetbrains.compose.web.dom.Text
+
+@Composable
+fun MainSection(onMenuClicked: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .id(Section.Home.id)
+            .maxWidth(SECTION_WIDTH.px),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        MainBackground()
+        MainContent(onMenuClicked = onMenuClicked)
+    }
+}
+
+@Composable
+fun MainBackground() {
+    Image(
+        modifier = Modifier
+            .fillMaxSize()
+            .objectFit(ObjectFit.Cover),
+        src = Res.Image.background,
+        description = "Background Image"
+    )
+}
+
+@Composable
+fun MainContent(onMenuClicked: () -> Unit) {
+    val breakpoint = rememberBreakpoint()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Header(onMenuClicked = onMenuClicked)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SimpleGrid(
+                modifier = Modifier.fillMaxWidth(
+                    if (breakpoint >= Breakpoint.MD) 80.percent
+                    else 90.percent
+                ),
+                numColumns = numColumns(base = 1, md = 2)
+            ) {
+                MainText(breakpoint = breakpoint)
+                MainImage()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainText(breakpoint: Breakpoint) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (breakpoint > Breakpoint.MD) {
+            SocialBar()
+        }
+        Column {
+            P(
+                attrs = Modifier
+                    .margin(topBottom = 0.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(if(breakpoint >= Breakpoint.LG) 45.px else 20.px)
+                    .fontWeight(FontWeight.Normal)
+                    .color(Theme.Primary.rgb)
+                    .toAttrs()
+            ) {
+                Text("Hello, I'm")
+            }
+            P(
+                attrs = Modifier
+                    .margin(top = 20.px, bottom = 0.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(if(breakpoint >= Breakpoint.LG) 68.px else 40.px)
+                    .fontWeight(FontWeight.Bolder)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text("Caleb Langat")
+            }
+            P(
+                attrs = Modifier
+                    .margin(top = 10.px, bottom = 5.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(20.px)
+                    .fontWeight(FontWeight.Bold)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text("Android Engineer and Open Source Enthusiast")
+            }
+            P(
+                attrs = Modifier
+                    .margin(bottom = 25.px)
+                    .maxWidth(400.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(15.px)
+                    .fontStyle(FontStyle.Italic)
+                    .fontWeight(FontWeight.Normal)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text(SHORT_INTRO)
+            }
+            Button(
+                attrs = MainButtonStyle.toModifier()
+                    .height(40.px)
+                    .border(width = 0.px)
+                    .borderRadius(r = 5.px)
+                    .backgroundColor(Theme.Primary.rgb)
+                    .color(Colors.White)
+                    .cursor(Cursor.Pointer)
+                    .toAttrs()
+            ) {
+                Link(
+                    modifier = Modifier
+                        .color(Colors.White)
+                        .textDecorationLine(TextDecorationLine.None),
+                    text = "Contact me",
+                    path = Section.Contact.path
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MainImage() {
+    Column(
+        modifier = Modifier.fillMaxSize(80.percent).fillMaxHeight(),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Image(
+            modifier = MainImageStyle.toModifier().fillMaxWidth(),
+            src = Res.Image.main,
+            description = "Main Image"
+        )
+    }
+}
---
+++ site/src/jsMain/kotlin/com/caleb/k/styles/MainSectionStyle.kt
@@ -0,0 +1,80 @@
+package com.caleb.k.styles
+
+import com.caleb.k.models.Theme
+import com.varabyte.kobweb.compose.css.CSSTransition
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.transform
+import com.varabyte.kobweb.compose.ui.modifiers.transition
+import com.varabyte.kobweb.compose.ui.modifiers.width
+import com.varabyte.kobweb.compose.ui.styleModifier
+import com.varabyte.kobweb.silk.components.style.ComponentStyle
+import com.varabyte.kobweb.silk.components.style.anyLink
+import com.varabyte.kobweb.silk.components.style.hover
+import org.jetbrains.compose.web.ExperimentalComposeWebApi
+import org.jetbrains.compose.web.css.*
+
+val NavigationItemStyle by ComponentStyle {
+    base {
+        Modifier
+            .color(Theme.Secondary.rgb)
+            .transition(CSSTransition(property = "color", duration = 200.ms))
+    }
+    anyLink {
+        Modifier.color(Theme.Secondary.rgb)
+    }
+    hover {
+        Modifier.color(Theme.Primary.rgb)
+    }
+}
+
+@OptIn(ExperimentalComposeWebApi::class)
+val LogoStyle by ComponentStyle {
+    base {
+        Modifier
+            .transform { rotate(0.deg) }
+            .transition(CSSTransition(property = "transform", duration = 200.ms))
+    }
+    hover {
+        Modifier
+            .transform { rotate((-10).deg) }
+    }
+}
+
+val SocialLinkStyle by ComponentStyle {
+    base {
+        Modifier
+            .color(Theme.Gray.rgb)
+            .transition(CSSTransition(property = "color", duration = 200.ms))
+    }
+    hover {
+        Modifier.color(Theme.Primary.rgb)
+    }
+}
+
+val MainButtonStyle by ComponentStyle {
+    base {
+        Modifier
+            .width(100.px)
+            .transition(CSSTransition(property = "width", duration = 200.ms))
+    }
+    hover {
+        Modifier.width(120.px)
+    }
+}
+
+@OptIn(ExperimentalComposeWebApi::class)
+val MainImageStyle by ComponentStyle {
+    base {
+        Modifier
+            .styleModifier {
+                filter { grayscale(100.percent) }
+            }
+            .transition(CSSTransition(property = "filter", duration = 200.ms))
+    }
+    hover {
+        Modifier.styleModifier {
+            filter { grayscale(0.percent) }
+        }
+    }
+}
