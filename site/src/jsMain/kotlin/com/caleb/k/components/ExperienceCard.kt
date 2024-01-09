3b8a8dad20eabe591d84db14e387966350c2b572
CalebK <langat.caleb25@gmail.com>
2024-01-09 15:03:29 +0300

    adds bullet points to Experience points

--- site/src/jsMain/kotlin/com/caleb/k/components/ExperienceCard.kt
+++ site/src/jsMain/kotlin/com/caleb/k/components/ExperienceCard.kt
@@ -41,7 +41,16 @@ fun ExperienceCard(
     ) {
         ExperienceDescription(
             active = active,
-            description = experience.description
+            bullet1 = experience.bullet1,
+            bullet2 = experience.bullet2,
+            bullet3 = experience.bullet3,
+            bullet4 = experience.bullet4,
+            bullet5 = experience.bullet5,
+            bullet6 = experience.bullet6,
+            bullet7 = experience.bullet7,
+            bullet8 = experience.bullet8,
+            bullet9 = experience.bullet9,
+            bullet10 = experience.bullet10,
         )
         ExperienceDetails(
             breakpoint = breakpoint,
@@ -55,9 +64,18 @@ fun ExperienceCard(
 @Composable
 fun ExperienceDescription(
     active: Boolean,
-    description: String,
-) {
-    Box(
+    bullet1:String,
+    bullet2: String,
+    bullet3: String,
+    bullet4: String,
+    bullet5: String,
+    bullet6: String,
+    bullet7: String,
+    bullet8: String,
+    bullet9: String,
+    bullet10: String,
+    ) {
+    Column (
         modifier = Modifier
             .fillMaxWidth()
             .margin(topBottom = 14.px)
@@ -74,8 +92,117 @@ fun ExperienceDescription(
                 .color(if (active) Colors.White else Theme.Secondary.rgb)
                 .toAttrs()
         ) {
-            Text(description)
+            Text(bullet1)
         }
+        P(
+            attrs = Modifier
+                .margin(topBottom = 0.px)
+                .fontFamily(FONT_FAMILY)
+                .fontSize(14.px)
+                .lineHeight(1.6)
+                .fontWeight(FontWeight.Normal)
+                .color(if (active) Colors.White else Theme.Secondary.rgb)
+                .toAttrs()
+        ) {
+            Text(bullet2)
+        }
+        P(
+            attrs = Modifier
+                .margin(topBottom = 0.px)
+                .fontFamily(FONT_FAMILY)
+                .fontSize(14.px)
+                .lineHeight(1.6)
+                .fontWeight(FontWeight.Normal)
+                .color(if (active) Colors.White else Theme.Secondary.rgb)
+                .toAttrs()
+        ) {
+            Text(bullet3)
+        }
+        P(
+            attrs = Modifier
+                .margin(topBottom = 0.px)
+                .fontFamily(FONT_FAMILY)
+                .fontSize(14.px)
+                .lineHeight(1.6)
+                .fontWeight(FontWeight.Normal)
+                .color(if (active) Colors.White else Theme.Secondary.rgb)
+                .toAttrs()
+        ) {
+            Text(bullet4)
+        }
+        P(
+            attrs = Modifier
+                .margin(topBottom = 0.px)
+                .fontFamily(FONT_FAMILY)
+                .fontSize(14.px)
+                .lineHeight(1.6)
+                .fontWeight(FontWeight.Normal)
+                .color(if (active) Colors.White else Theme.Secondary.rgb)
+                .toAttrs()
+        ) {
+            Text(bullet5)
+        }
+        P(
+            attrs = Modifier
+                .margin(topBottom = 0.px)
+                .fontFamily(FONT_FAMILY)
+                .fontSize(14.px)
+                .lineHeight(1.6)
+                .fontWeight(FontWeight.Normal)
+                .color(if (active) Colors.White else Theme.Secondary.rgb)
+                .toAttrs()
+        ) {
+            Text(bullet6)
+        }
+        P(
+            attrs = Modifier
+                .margin(topBottom = 0.px)
+                .fontFamily(FONT_FAMILY)
+                .fontSize(14.px)
+                .lineHeight(1.6)
+                .fontWeight(FontWeight.Normal)
+                .color(if (active) Colors.White else Theme.Secondary.rgb)
+                .toAttrs()
+        ) {
+            Text(bullet7)
+        }
+        P(
+            attrs = Modifier
+                .margin(topBottom = 0.px)
+                .fontFamily(FONT_FAMILY)
+                .fontSize(14.px)
+                .lineHeight(1.6)
+                .fontWeight(FontWeight.Normal)
+                .color(if (active) Colors.White else Theme.Secondary.rgb)
+                .toAttrs()
+        ) {
+            Text(bullet8)
+        }
+        P(
+            attrs = Modifier
+                .margin(topBottom = 0.px)
+                .fontFamily(FONT_FAMILY)
+                .fontSize(14.px)
+                .lineHeight(1.6)
+                .fontWeight(FontWeight.Normal)
+                .color(if (active) Colors.White else Theme.Secondary.rgb)
+                .toAttrs()
+        ) {
+            Text(bullet9)
+        }
+        P(
+            attrs = Modifier
+                .margin(topBottom = 0.px)
+                .fontFamily(FONT_FAMILY)
+                .fontSize(14.px)
+                .lineHeight(1.6)
+                .fontWeight(FontWeight.Normal)
+                .color(if (active) Colors.White else Theme.Secondary.rgb)
+                .toAttrs()
+        ) {
+            Text(bullet10)
+        }
+
     }
 }
 
